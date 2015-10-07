d6af4a4691257f7b8c579d8d25f409ccc709798e
Jason Puglisi <jason@puglisi.im>
2015-10-07 14:48:34 -0400

    Added Rville Spirit website and comments for planned additions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
+# [Website] Road Surfer - https://www.roadsurfer.com/
 Project.create(title: 'Road Surfer', description: 'Road Surfer is an upstart
   company that sells transportation devices with focus on enjoyment and
   portability', image: 'road-surfer.png', links: {
   'web' => 'https://www.surftheroad.com/'}, kind: 'website')
+
+# [Website] Rville Spirit - https://www.rvillespirit.com/
+Project.create(title: 'Rville Spirit', description: 'Rville Spirit is
+  Robbinsville High School\'s official online spiritwear store',
+  image: 'rville-spirit.png', links: {
+  'web' => 'https://www.rvillespirit.com/',
+  'github' => 'https://github.com/JasonPuglisi/rville-spirit-website'},
+  kind: 'website')
+
+# [Website] FITspiration - https://www.findanewmax.com/
+
+# [Website] Build Your Own Craving
+
+# [Utility] Snow IRC Bot
+
+# [Utility] MyPKS API
+
+# [Utility] Plug.dj Scrobbler
+
+# [Pebble Watchface] Helvetica Text
+
+# [Pebble Watchface] Slammin' Watchface
 
