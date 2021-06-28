23a0c2fc7e2987827e8e7b3c2b03be8a32e7ee6f
Jason Puglisi <hello@jasonpuglisi.com>
2021-06-28 16:43:53 -0700

    Update dependencies (#32)

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ let gulp = require('gulp');
 let autoprefixer = require('gulp-autoprefixer');
 let browserSync = require('browser-sync').create();
 let htmlmin = require('gulp-htmlmin');
-let sass = require('gulp-sass');
+let sass = require('gulp-sass')(require('sass'));
 let sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build-html', function (done) {
